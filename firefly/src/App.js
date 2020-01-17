dae6177d091eaa86666e3dd6de0718aa3f4eaa7e
UsamiHaruuu <lanborui@Outlook.com>
2020-01-17 14:25:37 -0600

    brian's test

--- firefly/src/App.js
+++ firefly/src/App.js
@@ -1,13 +1,13 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
-
   return (
     <div>
+      <p>Brian's branch</p>
       <p> Joo's branch testing </p>
-      <p> Lucy's change</p><
-     /div>
+      <p> Lucy's change</p>
+    </div>
   );
 }
 
